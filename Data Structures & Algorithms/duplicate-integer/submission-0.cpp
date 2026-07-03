046a386d943454adace0bf8c3ce534af84881d82
Sahil18official <sahilsahanioffical18@gmail.com>
2026-07-03 18:21:13 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        set<int> s;
+        for(int i=0;i<nums.size();i++){
+            s.insert(nums[i]);
+        }
+        
+        return s.size()!=nums.size();
+        
+    }
+};
